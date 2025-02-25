e6f9265322eb68937d267172489f306797f7c54f
Francisco Natareno <francisco.natareno@outlook.com>
2025-02-24 18:06:37 -0600

    contenido de la sesion 2

---
+++ sesion002/statics/js/index.js
@@ -0,0 +1,30 @@
+let contador = -1
+let fizzBuzz = []
+
+function pintarEnPantalla() {
+  //Encargada de modificar el paragrahp "fizzBuzzResult"
+}
+
+function esFizzOEsBuzz(num) {
+  // Encrgad de devolver, fizz, buzz o el numero
+  // si es multiplo de 3, devolver fizz
+  // si es multiplo de 5, devolver buzz
+  // si es multiplo de 3 y 5, devolver fizzBuzz
+}
+
+function siguiente(){
+  // añadir un siguiente numero a la array fizzBuzz
+  contador += 1;
+  console.log(contador+1);
+}
+
+function siguienteN(rondas){
+  // encargada de llamar de manera iterada, a la funcion siguiente
+}
+
+function limpiar(){
+  //encargada de vaciar la lista fizzBuzz, y limpiar el paragrahp
+  contador = -1;
+  fizzBuzz.length = 0;
+
+}
